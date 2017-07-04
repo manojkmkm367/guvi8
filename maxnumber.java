561fb461a72777133f9850042655f8bb6277740c
manojkmkm367 <manojkmkm367@gmail.com>
2017-07-04 15:51:30 +0530

    Create maxnumber.java

---
+++ maxnumber.java
@@ -0,0 +1,21 @@
+
+	import java.io.IOException;
+
+import java.util.*;
+	public class Number {
+		  public static void main(String[] args)throws IOException
+		  {
+			  Scanner in=new Scanner(System.in);
+			  int a=in.nextInt(); int b=in.nextInt(); int c=in.nextInt();
+			  int max=0;
+			 if(a<b)
+				 a=b;
+			
+			if(a<c)
+				a=c;
+		
+				  System.out.println(a);
+			
+			  
+		  }
+}
