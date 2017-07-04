4c7d4d43df553c67e6b184f0fefba065d55c0690
manojkmkm367 <manojkmkm367@gmail.com>
2017-07-04 16:00:04 +0530

    Create printhello.java

---
+++ printhello.java
@@ -0,0 +1,13 @@
+
+	import java.io.IOException;
+
+import java.util.*;
+	public class Number {
+		  public static void main(String[] args)throws IOException
+		  {
+			 for(int i=0;i<5;i++)
+				 System.out.println("Hello");
+			
+			  
+		  }
+}
