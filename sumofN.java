3b13f36a2b1a3a8d7e158057f6805991fe1801f2
manojkmkm367 <manojkmkm367@gmail.com>
2017-07-04 16:42:54 +0530

    Create sumofN.java

---
+++ sumofN.java
@@ -0,0 +1,17 @@
+
+	import java.io.IOException;
+
+import java.util.*;
+	public class Number {
+		  public static void main(String[] args)throws IOException
+		  {
+			  Scanner in=new Scanner(System.in);
+			  int n=in.nextInt(); 
+			  int x=0;
+			for(int i=0;i<n;i++)
+				x=x+i+1;
+			
+			
+			  System.out.println(x);
+		  }
+}
